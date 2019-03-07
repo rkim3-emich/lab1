ae019b1fd5e6ab610fcc9876c97af2a2c26cfa77
rkim3-emich <48301821+rkim3-emich@users.noreply.github.com>
2019-03-06 20:20:36 -0500

    Add files via upload

---
+++ lab1/IntManips.java
@@ -0,0 +1,34 @@
+
+/**
+ * Outputs simple computations.
+ *
+ * @author (Ryan Kim)
+ * @version (1-16-19)
+ */
+public class IntManips
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Programmer: Ryan Kim");
+        System.out.println("Course:     COSC 111, Winter 2019");
+        System.out.println("Lab#:       1, part #2");
+        System.out.println("Due date:   1-16-2019\n");
+        System.out.println("10 - 3 = " + (10-3));
+        System.out.println("10 / 3 = " + (10/3));
+        System.out.println("10 * 3 = " + (10*3));
+        System.out.println("10 % 3 = " + (10%3));
+        System.out.println("(10 + 3) / 2 = " + ((10 + 3)/2));
+    }
+}
+/*
+Programmer: Ryan Kim
+Course:     COSC 111, Winter 2019
+Lab#:       1, part #2
+Due date:   1-16-2019
+
+10 - 3 = 7
+10 / 3 = 3
+10 * 3 = 30
+10 % 3 = 1
+(10 + 3) / 2 = 6
+*/
---
+++ lab1/Poem.java
@@ -0,0 +1,53 @@
+
+/**
+ * Outputs a poem. Simple program to learn about escape sequences.
+ *
+ * @author (Ryan Kim)
+ * @version (1-16-19)
+ */
+public class Poem
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Programmer: Ryan Kim");
+        System.out.println("Course:     COSC 111, Winter 2019");
+        System.out.println("Lab#:       1, part #1");
+        System.out.println("Due date:   1-16-2019\n\n");
+        System.out.println("Roses are red \n");
+        System.out.println("++++++++++++++++++++++++++");
+        System.out.println("+  Roses are red         +");
+        System.out.println("+  Violets are \"blue\"    +");
+        System.out.println("+  Chocolate is better   +");
+        System.out.println("+  By a \'pound\' or two!! +");
+        System.out.println("++++++++++++++++++++++++++ \n");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("\\  Roses are red         \\");
+        System.out.println("\\  Violets are \"blue\"    \\");
+        System.out.println("\\  Chocolate is better   \\");
+        System.out.println("\\  By a \'pound\' or two!! \\");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
+/*
+Programmer: Ryan Kim
+Course:     COSC 111, Winter 2019
+Lab#:       1, part #1
+Due date:   1-16-2019
+
+
+Roses are red 
+
+++++++++++++++++++++++++++
++  Roses are red         +
++  Violets are "blue"    +
++  Chocolate is better   +
++  By a 'pound' or two!! +
+++++++++++++++++++++++++++ 
+
+\\\\\\\\\\\\\\\\\\\\\\\\\\
+\  Roses are red         \
+\  Violets are "blue"    \
+\  Chocolate is better   \
+\  By a 'pound' or two!! \
+\\\\\\\\\\\\\\\\\\\\\\\\\\
+*/
